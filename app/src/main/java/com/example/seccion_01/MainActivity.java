7ad2727150f00c14b06c667a2186f8cf03594684
Noe Solis <noesolism@hotmail.com>
2019-04-28 11:33:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/seccion_01/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.seccion_01;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private final String GREETER="Hola vengo de otra Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.buttonMain);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("Valor enviado",GREETER);
+            }
+        });
+
+
+    }
+}
